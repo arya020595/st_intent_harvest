ead7b91944363e44c4ab64cddee95667fcd65f22
arya020595 <aryarifqipratama@gmail.com>
2025-10-24 06:50:33 +0700

    Refactor routes to organize namespaces and resources for better clarity and maintainability

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
+  
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
@@ -7,5 +8,46 @@ Rails.application.routes.draw do
   get "up" => "rails/health#show", as: :rails_health_check
 
   # Defines the root path route ("/")
-  # root "posts#index"
+  root "dashboard#index"
+  
+  # Dashboard
+  get "dashboard", to: "dashboard#index"
+  
+  # Work Order Namespace
+  namespace :work_order do
+    resources :details
+    resources :approvals, only: [:index, :show, :update] do
+      member do
+        patch :approve
+        patch :reject
+      end
+    end
+    resources :pay_calculations
+  end
+  
+  # Payslips
+  resources :payslips, only: [:index, :show] do
+    collection do
+      get :export
+    end
+  end
+  
+  # Inventory
+  resources :inventories
+  
+  # Work Profile
+  resources :work_profiles
+  
+  # Master Data Namespace
+  namespace :master_data do
+    resources :workers
+    resources :work_order_rates
+    resources :blocks
+  end
+  
+  # User Management Namespace
+  namespace :user_management do
+    resources :roles
+    resources :users
+  end
 end
