2aa69c33c2f0eb53b67a7162a8d2d3edaa7e0878
arya020595 <aryarifqipratama@gmail.com>
2025-10-29 10:49:41 +0700

    Refactor dashboard layout and enhance user interface with Bootstrap components; update work order display and add flash message support.

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,3 +1,6 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
 import "@hotwired/turbo-rails"
 import "controllers"
+
+// Bootstrap JavaScript - auto-initializes data-bs-* attributes
+import "bootstrap"
