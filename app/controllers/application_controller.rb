f6a493303d29fc9e8e9cf3be55db475a2b5baeca
arya020595 <aryarifqipratama@gmail.com>
2025-10-30 21:22:23 +0700

    Fix formatting inconsistencies in ApplicationController

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,16 +1,17 @@
 class ApplicationController < ActionController::Base
   include Pundit::Authorization
+  include Pagy::Backend
 
   before_action :authenticate_user!
   before_action :set_current_user
-  
+
   # Smart layout switching: dashboard for authenticated pages, application for public pages
   layout :set_layout
 
   rescue_from Pundit::NotAuthorizedError, with: :user_not_authorized
 
   private
-  
+
   def set_layout
     # Use clean layout for Devise controllers (login, signup, password reset)
     # Use dashboard layout for all other authenticated pages
@@ -22,7 +23,7 @@ class ApplicationController < ActionController::Base
   end
 
   def user_not_authorized
-    flash[:alert] = "You are not authorized to perform this action."
+    flash[:alert] = 'You are not authorized to perform this action.'
     redirect_to(request.referrer || root_path)
   end
 end
