bf0a9f93c5aecbbe0506e1b746707b9c80a2778b
arya020595 <aryarifqipratama@gmail.com>
2025-10-31 13:07:38 +0700

    Refactor sidebar menu to conditionally display items based on user permissions

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -10,4 +10,13 @@ module ApplicationHelper
   def active_controller?(*controllers)
     controllers.any? { |controller| controller_path.start_with?(controller) }
   end
+
+  # Check if user has permission to view a menu item
+  def can_view_menu?(subject, action = 'index')
+    return true unless current_user # Show to guests (will be caught by authentication)
+    return true if current_user.role&.name&.downcase == 'superadmin' # Superadmin sees all
+
+    permission_checker = PermissionChecker.new(current_user)
+    permission_checker.allowed?(action, subject)
+  end
 end
