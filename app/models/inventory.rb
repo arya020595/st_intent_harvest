9aed2f98a729e32dcd58f0461f12ef171b255935
arya020595 <aryarifqipratama@gmail.com>
2025-11-10 08:11:39 +0700

    Refactor validations in Inventory model for improved readability and consistency

--- app/models/inventory.rb
+++ app/models/inventory.rb
@@ -3,15 +3,29 @@ class Inventory < ApplicationRecord
   belongs_to :unit, optional: true
   has_many :work_order_items, dependent: :nullify
 
-  validates :name, presence: { message: "Please enter the inventory name" }
-  validates :category_id, presence: { message: "Please select a category" }, if: -> { category_id.present? || category_id.nil? }
-  validates :unit_id, presence: { message: "Please select a unit" }, if: -> { unit_id.present? || unit_id.nil? }
-  validates :stock_quantity, numericality: { greater_than_or_equal_to: 0, message: "Quantity must be 0 or more" }, allow_nil: true
-  validates :price, numericality: { greater_than_or_equal_to: 0, message: "Price must be 0 or more" }, allow_nil: true
-  validates :supplier, presence: { message: "Please enter supplier name" }, if: -> { supplier.present? || supplier.nil? }
-  validates :input_date, presence: { message: "Please select an input date" }, if: -> { input_date.present? || input_date.nil? }
-end
+  validates :name, presence: { message: 'Please enter the inventory name' }
+  validates :category_id, presence: { message: 'Please select a category' }, if: lambda {
+    category_id.present? || category_id.nil?
+  }
+  validates :unit_id, presence: { message: 'Please select a unit' }, if: -> { unit_id.present? || unit_id.nil? }
+  validates :stock_quantity, numericality: { greater_than_or_equal_to: 0, message: 'Quantity must be 0 or more' },
+                             allow_nil: true
+  validates :price, numericality: { greater_than_or_equal_to: 0, message: 'Price must be 0 or more' }, allow_nil: true
+  validates :supplier, presence: { message: 'Please enter supplier name' }, if: lambda {
+    supplier.present? || supplier.nil?
+  }
+  validates :input_date, presence: { message: 'Please select an input date' }, if: lambda {
+    input_date.present? || input_date.nil?
+  }
 
+  def self.ransackable_attributes(_auth_object = nil)
+    %w[id name category_id unit_id price currency stock_quantity supplier input_date created_at updated_at]
+  end
+
+  def self.ransackable_associations(_auth_object = nil)
+    %w[category unit work_order_items]
+  end
+end
 
 # == Schema Information
 #
