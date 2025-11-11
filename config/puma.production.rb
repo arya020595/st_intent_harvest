26edf1973fafd3b384dd5c8282d73661ee2ad6bf
arya020595 <aryarifqipratama@gmail.com>
2025-11-11 13:14:23 +0700

    Update Dockerfile.production and add puma configuration for production

---
+++ config/puma.production.rb
@@ -0,0 +1,23 @@
+# config/puma.production.rb
+
+max_threads_count = ENV.fetch('RAILS_MAX_THREADS', 5)
+min_threads_count = ENV.fetch('RAILS_MIN_THREADS') { max_threads_count }
+threads min_threads_count, max_threads_count
+
+workers ENV.fetch('WEB_CONCURRENCY', 2)
+
+preload_app!
+
+port ENV.fetch('PORT', 3000)
+environment ENV.fetch('RAILS_ENV', 'production')
+
+plugin :tmp_restart
+plugin :solid_queue if ENV['SOLID_QUEUE_IN_PUMA']
+
+worker_timeout 30
+worker_shutdown_timeout 30
+
+pidfile ENV.fetch('PIDFILE', 'tmp/pids/server.pid')
+state_path 'tmp/pids/puma.state'
+
+stdout_redirect 'log/puma.stdout.log', 'log/puma.stderr.log', true unless ENV['RAILS_LOG_TO_STDOUT'] == 'true'
