b146c72d3f538f34c51f30764314e205b0e8612b
arya020595 <aryarifqipratama@gmail.com>
2025-11-14 09:15:02 +0700

    Refactor migration to improve comments for work_month and work_days fields

--- db/migrate/20251114084868_add_mandays_fields.rb
+++ db/migrate/20251114084868_add_mandays_fields.rb
@@ -2,10 +2,12 @@
 
 class AddMandaysFields < ActiveRecord::Migration[7.0]
   def change
+    # Add work_month to work_orders for tracking which month the work was done
     add_column :work_orders, :work_month, :date,
                comment: 'First day of the month for Mandays calculation'
 
-    add_column :work_order_workers, :work_days, :integer, default: 0, null: false,
-                                                          comment: 'How many days worker works in given month'
+    # Add work_days to work_order_workers to track days worked per worker per month
+    add_column :work_order_workers, :work_days, :integer,
+               default: 0, null: false, comment: 'How many days worker works in given month'
   end
 end
