2320b8a19f95a21f92da918346823f1b7c0cab17
arya020595 <aryarifqipratama@gmail.com>
2025-11-14 09:04:17 +0700

    Add migration for mandays fields and rate type in work orders

---
+++ db/migrate/20251114084868_add_mandays_fields.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+class AddMandaysFields < ActiveRecord::Migration[7.0]
+  def change
+    add_column :work_orders, :work_month, :date,
+               comment: 'First day of the month for Mandays calculation'
+
+    add_column :work_order_workers, :work_days, :integer, default: 0, null: false,
+                                                          comment: 'How many days worker works in given month'
+  end
+end
---
+++ db/migrate/20251114084869_add_rate_type_to_work_order_rates.rb
@@ -0,0 +1,9 @@
+# frozen_string_literal: true
+
+class AddRateTypeToWorkOrderRates < ActiveRecord::Migration[7.0]
+  def change
+    add_column :work_order_rates, :work_order_rate_type, :string,
+               default: 'normal',
+               comment: 'Type of work order rate: normal (all fields), resources (resource fields only), work_days (worker details only)'
+  end
+end
