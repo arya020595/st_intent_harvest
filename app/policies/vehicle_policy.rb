321715b14d55cf3d97d42de75a0add57467db4b6
arya020595 <aryarifqipratama@gmail.com>
2025-11-25 15:19:07 +0700

    Remove unused policy files: BlockPolicy, VehiclePolicy, and WorkProfilePolicy

--- app/policies/vehicle_policy.rb
+++
@@ -1,9 +0,0 @@
-# frozen_string_literal: true
-
-class VehiclePolicy < ApplicationPolicy
-  # Inherits all default behavior from ApplicationPolicy
-
-  class Scope < ApplicationPolicy::Scope
-    # Inherits default scope behavior
-  end
-end
