c4d25032f94e05f35045371bee7e9279dc089a81
arya020595 <aryarifqipratama@gmail.com>
2025-11-26 13:43:23 +0700

    Refactor work order deletion process and improve policy checks in views; streamline flash message handling

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -21,4 +21,21 @@ module ApplicationHelper
 
     current_user.has_permission?(permission_code)
   end
+
+  # Returns a policy instance for the given record and policy class
+  # This follows the Single Responsibility Principle by centralizing policy instantiation
+  # and makes it easy to reuse across the entire application
+  #
+  # @param record [ActiveRecord::Base] the record to authorize
+  # @param policy_class [Class] the policy class to use
+  # @return [ApplicationPolicy] policy instance
+  #
+  # Example usage in views:
+  #   <% policy = record_policy(work_order, WorkOrders::DetailPolicy) %>
+  #   <% if policy.edit? %>
+  #     <%= link_to "Edit", edit_path(work_order) %>
+  #   <% end %>
+  def record_policy(record, policy_class)
+    policy_class.new(current_user, record)
+  end
 end
