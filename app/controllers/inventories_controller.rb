df456260b41829067c1c25472dd67384359cb865
arya020595 <aryarifqipratama@gmail.com>
2025-12-01 14:21:47 +0700

    Refactor InventoriesController to enhance Turbo frame handling and improve flash message responses

--- app/controllers/inventories_controller.rb
+++ app/controllers/inventories_controller.rb
@@ -17,41 +17,84 @@ class InventoriesController < ApplicationController
   def new
     @inventory = Inventory.new
     authorize @inventory
+
+    if turbo_frame_request?
+      render layout: false
+    else
+      redirect_to inventories_path
+    end
   end
 
   # GET /inventories/:id
   def show
     authorize @inventory
+
+    if turbo_frame_request?
+      render layout: false
+    else
+      redirect_to inventories_path
+    end
   end
 
   # GET /inventories/:id/edit
   def edit
     authorize @inventory
+
+    if turbo_frame_request?
+      render layout: false
+    else
+      redirect_to inventories_path
+    end
   end
 
   # GET /inventories/:id/confirm_delete
   def confirm_delete
     authorize @inventory
+
+    # Only render the modal if Turbo frame request
+    if turbo_frame_request?
+      render layout: false
+    else
+      redirect_to inventories_path
+    end
   end
 
   def create
     @inventory = Inventory.new(inventory_params)
     authorize @inventory
 
-    if @inventory.save
-      flash[:notice] = 'Inventory added successfully!'
-    else
-      flash[:alert] = 'Failed to add inventory.'
+    respond_to do |format|
+      if @inventory.save
+        format.turbo_stream do
+          flash.now[:notice] = 'Inventory was successfully created.'
+        end
+        format.html { redirect_to inventories_path, notice: 'Inventory was successfully created.' }
+      else
+        format.turbo_stream do
+          render turbo_stream: turbo_stream.replace('modal', partial: 'inventories/form', locals: { inventory: @inventory }),
+                 status: :unprocessable_entity
+        end
+        format.html { render :new, status: :unprocessable_entity }
+      end
     end
   end
 
   def update
     authorize @inventory
 
-    if @inventory.update(inventory_params)
-      flash[:notice] = 'Inventory updated successfully!'
-    else
-      flash[:alert] = 'Failed to update inventory.'
+    respond_to do |format|
+      if @inventory.update(inventory_params)
+        format.turbo_stream do
+          flash.now[:notice] = 'Inventory was successfully updated.'
+        end
+        format.html { redirect_to inventories_path, notice: 'Inventory was successfully updated.' }
+      else
+        format.turbo_stream do
+          render turbo_stream: turbo_stream.replace('modal', partial: 'inventories/form', locals: { inventory: @inventory }),
+                 status: :unprocessable_entity
+        end
+        format.html { render :edit, status: :unprocessable_entity }
+      end
     end
   end
 
@@ -59,9 +102,14 @@ class InventoriesController < ApplicationController
     authorize @inventory
 
     if @inventory.destroy
-      flash[:notice] = 'Inventory deleted successfully!'
+      respond_to do |format|
+        format.turbo_stream do
+          flash.now[:notice] = 'Inventory deleted successfully.'
+        end
+        format.html { redirect_to inventories_path, notice: 'Inventory deleted successfully.' }
+      end
     else
-      flash[:alert] = 'Failed to delete inventory.'
+      redirect_to inventories_path, alert: 'Failed to delete inventory.'
     end
   end
 
@@ -69,7 +117,14 @@ class InventoriesController < ApplicationController
 
   # Loads the inventory item using the ID in the URL
   def set_inventory
-    @inventory = Inventory.find(params[:id])
+    @inventory = Inventory.find_by(id: params[:id])
+    return if @inventory.present?
+
+    if turbo_frame_request?
+      render turbo_stream: turbo_stream.replace('modal', ''), status: :ok
+    else
+      redirect_to inventories_path
+    end
   end
 
   # Only allow a list of trusted parameters through
