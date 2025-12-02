c5e599520a7e7855f6c07d795069b1a421fde321
arya020595 <aryarifqipratama@gmail.com>
2025-12-02 16:42:25 +0700

    Refactor migration to add vehicle_id column with concurrent index creation

--- db/migrate/20251202083051_add_vehicle_to_work_orders.rb
+++ db/migrate/20251202083051_add_vehicle_to_work_orders.rb
@@ -3,7 +3,9 @@ class AddVehicleToWorkOrders < ActiveRecord::Migration[8.1]
 
   def change
     # Add vehicle_id column if it does not exist, then add foreign key
-    add_reference :work_orders, :vehicle, index: true, foreign_key: false unless column_exists?(:work_orders, :vehicle_id)
+    unless column_exists?(:work_orders, :vehicle_id)
+      add_reference :work_orders, :vehicle, index: { algorithm: :concurrently }, foreign_key: false
+    end
     add_foreign_key :work_orders, :vehicles, validate: false unless foreign_key_exists?(:work_orders, :vehicles)
   end
 end
